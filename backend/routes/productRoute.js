714a63d39bb0f679c144516a69c4224141719fd2
Rajnish0202 <rajnish.0202kumar@gmail.com>
2022-11-30 22:48:26 +0530

    reviews controller

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,6 +4,9 @@ const {
   updateProduct,
   deleteProduct,
   getProductDetails,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
 } = require('../controllers/productController');
 
 const express = require('express');
@@ -22,4 +25,7 @@ router
 
 router.route('/product/:id').get(getProductDetails);
 
+router.route('/review').put(isAuthenticatedUser, createProductReview);
+
+router.route('/reviews').get(getProductReviews).delete(isAuthenticatedUser, deleteReview);
 module.exports = router;
