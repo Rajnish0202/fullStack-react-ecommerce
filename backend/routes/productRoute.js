a25152ac4722c66db2f8b6425cb7eb9cfa192f9b
Rajnish0202 <rajnish.0202kumar@gmail.com>
2022-12-01 12:35:06 +0530

    orderController

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const {
   getAllProducts,
   createProduct,
@@ -9,7 +10,6 @@ const {
   deleteReview,
 } = require('../controllers/productController');
 
-const express = require('express');
 const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
 const router = express.Router();
